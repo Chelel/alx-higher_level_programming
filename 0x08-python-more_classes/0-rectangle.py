5a32ecb2463bac91cd1a35adb293a9a936275412
Chelel <korir.chelel@gmail.com>
2023-04-03 05:34:03 -0700

    file edits

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ create an empty class Rectangle that defines a rectangle"""
+"""Defines a rectangle"""
 
 class Rectangle:
-    """Epmpty class Rectangle"""
+    """ Represents a  class Rectangle."""
     pass
