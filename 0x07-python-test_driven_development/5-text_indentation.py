226f0c220fadc6c366674a48dfd8c2b5db2ac7f5
Chelel <korir.chelel@gmail.com>
2023-04-07 08:31:28 -0700

    fixing the errors

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 # Text indentation
 
+''' A function that prints a text with 2 new lines after 
+each of these characters: ., ? and : '''
+
+
 def text_indentation(text):
     """defines a function that prints a text with 2 new lines
     after each of these characters: ., ? and :"""
@@ -8,8 +12,9 @@ def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError('text must be a string')
 
-    new_text = text.replace('.', '.\n')
-    new_text = new_text.replace('?', '?\n')
-    new_text = new_text.replace(':', ':\n')
-    new_text = new_text.replace('\n ', '\n')
+    new_text = text.replace('.', '.\n\n')
+    new_text = new_text.replace('?', '?\n\n')
+    new_text = new_text.replace(':', ':\n\n')
+    new_text = new_text.replace('\n ', '\n\n')
+
     print(new_text)
