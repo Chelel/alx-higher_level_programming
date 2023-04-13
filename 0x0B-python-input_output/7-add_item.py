8e703d2143c6ac29c423d4770c206cdcc5d14893
Chelel <korir.chelel@gmail.com>
2023-04-13 12:46:24 -0700

    Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,16 +5,15 @@ Load, add, save"""
 
 import os
 from sys import argv
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
-argv_n = argv[1:]
+save_file = __import__('5-save_to_json_file').save_to_json_file
+load_file = __import__('6-load_from_json_file').load_from_json_file
 
 try:
-    txt_list = json_file("add_item.json")
-except FileNotFoundError:
-    txt_list = []
-finally:
-    for arg in argv_n:
-        txt_list.append(arg)
-    save_to_json_file(txt_list, "add_item.json")
+    json_list = load_file('add_item.json')
+except (ValueError, FileNotFoundError):
+    json_list = []
+
+for item in argv[1:]:
+    json_list.append(item)
+
+save_file(json_list, 'add_item.json')
