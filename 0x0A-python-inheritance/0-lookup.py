7733d3516bc797c81e1b16adcda169020f13daad
Chelel <korir.chelel@gmail.com>
2023-06-12 05:04:39 -0700

    python inheritance

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 
+"""eturns the list of available attributes and methods of an object:
+
+Prototype: def lookup(obj):
+Returns a list object"""
+
+
 def lookup(obj):
     """a function that returns the list of available
     attributes and methods of an object"""
