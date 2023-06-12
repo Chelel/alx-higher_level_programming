3a2ac57927eef0383e35b825ce2c2f0f1067e628
Chelel <korir.chelel@gmail.com>
2023-06-12 05:06:31 -0700

    class MyList that inherits from list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""a class MyList that inherits from list:
+
+Public instance method: def print_sorted(self): that prints the list, but sorted (ascending sort)
+You can assume that all the elements of the list will be of type int"""
+
 
 class MyList(list):
     """class MyList that inherits from list"""
