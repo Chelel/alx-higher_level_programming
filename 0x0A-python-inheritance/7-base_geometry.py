d983d016506b8ab577a5c8236bbbea5821f89a51
Chelel <korir.chelel@gmail.com>
2023-06-12 06:29:51 -0700

    Integer validator mandatory

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Integer validator"""
+
+
+class BaseGeometry:
+    """ a class BaseGeometry"""
+    def area(self):
+        """Public instance method that raises an Exception
+        area() is not implemented"""
+        raise Exception("area() is not implemented")
+
+
+    def integer_validator(self, name, value):
+        "Public instance method that validates value"""
+        if not isinstance(value, int):
+            raise TypeError(f'{name}  must be an integer')
+        if value <= 0:
+            raise ValueError(f'{name} must be greater than 0')
+
