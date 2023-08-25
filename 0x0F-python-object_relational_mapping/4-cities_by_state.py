44f7854076addbecaeee9a8050bbe929485bb74c
Chelel <korir.chelel@gmail.com>
2023-08-25 03:23:46 -0700

    lists all cities of that state, using the database

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""lists all cities from the database 
+  take 3 arguments: mysql username, mysql password and database name"""
+
+
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities \
+                JOIN states ON cities.state_id = states.id \
+                ORDER BY cities.id")
+    [print(cities) for cities in cur.fetchall()]
