bdff0857d15887ace5d73aebc06ffff087dcfad4
Chelel <korir.chelel@gmail.com>
2023-08-25 04:21:42 -0700

    First state model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -9,6 +9,7 @@ Base = declarative_base()
 
 
 class State(Base):
+    """class state that inherits from base"""
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
